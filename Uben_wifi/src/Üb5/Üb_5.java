a799bf24f6848cdd66c95c675e232410a2ecd9c1
BrigittaJuh <brigittaj91@gmail.com>
2024-07-04 00:51:57 +0200

    fünfte

---
+++ Uben_wifi/src/Üb5/Üb_5.java
@@ -0,0 +1,19 @@
+package Üb5;
+
+public class Üb_5 {
+    public static void main (String[] args){
+
+        int number = 0;
+
+        if (number < 0) {
+            System.out.println("Der Zahl ist negativ");
+        } else if  (number > 0) {
+            System.out.println("Der Zahl ist pozitiv");
+
+        } else {
+            System.out.println("Der Zahl ist null");
+        }
+
+    }
+
+}
