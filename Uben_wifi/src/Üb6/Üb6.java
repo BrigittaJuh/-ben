d17447d635a991905eaf45c8b4060f06450501f6
BrigittaJuh <brigittaj91@gmail.com>
2024-07-04 01:02:29 +0200

    sechste

---
+++ Uben_wifi/src/Üb6/Üb6.java
@@ -0,0 +1,14 @@
+package Üb6;
+
+public class Üb6 {
+    public static void main(String[] args){
+        int numb1 = 9;
+        int numb2 = 7;
+
+        if (numb1 <= numb2){
+            System.out.println("Dise Zahl: " + numb2 + " grösser als :" + numb1);
+        } else {
+            System.out.println("Dise Zahl: " + numb1 + " grösser als :" + numb2);
+        }
+    }
+}
