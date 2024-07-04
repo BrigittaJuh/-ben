ac62eff53a87f2dcc9a8c09e0cca9690944a05a9
BrigittaJuh <brigittaj91@gmail.com>
2024-07-04 15:38:46 +0200

    Siebte Übung

---
+++ Uben_wifi/src/Üb7/Test.java
@@ -0,0 +1,13 @@
+package Üb7;
+
+public class Test {
+    public static void main(String[] args){
+
+        Tier tier1 = new Tier("Diego");
+        Tier tier2 = new Tier("Zuba");
+
+        System.out.println("1. Tier Name :" + tier1.getName());
+        System.out.println("2. Tier Name :" + tier2.getName());
+
+    }
+}
---
+++ Uben_wifi/src/Üb7/Tier.java
@@ -0,0 +1,12 @@
+package Üb7;
+
+public class Tier {
+        private String name ;
+        public Tier(String name){
+            this.name = name;
+        }
+
+    public String getName() {
+        return name;
+    }
+}
