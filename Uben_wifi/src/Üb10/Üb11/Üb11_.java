b97e04b855cf820c08625cd4f2923e71289eee9e
BrigittaJuh <brigittaj91@gmail.com>
2024-07-07 00:38:15 +0200

    üb11

---
+++ Uben_wifi/src/Üb10/Üb11/Üb11_.java
@@ -0,0 +1,14 @@
+package Üb10.Üb11;
+
+public class Üb11_ {
+    public static void main(String[] args) {
+        int[] numbers = {5,1,1,1,1,1,1,1,1,2};
+        int sum = 0;
+        for (int i = 0; i < 10; i++) {
+            sum += numbers[i];
+        }
+        double average = (double)sum / 10;
+
+        System.out.println(average);
+    }
+}
---
+++ Uben_wifi/src/Üb10/Üb_10_.java
@@ -0,0 +1,12 @@
+package Üb10;
+
+public class Üb_10_ {
+    public static void main(String[] args) {
+        int[] number = {5,5,5,5,5};
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            sum += number[i];
+        }
+        System.out.println(sum );
+    }
+}
